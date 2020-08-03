5a0ec74a28ead42cbf65633e88e85b2682c1acff
p.jeevana <p.jeevana@CHNMCT302816D.ad.infosys.com>
2020-08-03 11:46:01 +0530

    login page testcases updated

--- Newproject/src/test/java/Testpages/Loginpagetest.java
+++ Newproject/src/test/java/Testpages/Loginpagetest.java
@@ -1,16 +1,30 @@
 package Testpages;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Loginpagetest {
 	
 	@BeforeTest
 	
-	public static void initialization() {
+	public static void loginintoapplication() {
+		System.out.println("successfully login");
 		
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println(" verify the logo");
 		
 	}
 	
 	
-
+@BeforeTest
+	
+	public static void teardown() {
+	System.out.println("successfully logout");
+	
+		
+	}
+	
 }
